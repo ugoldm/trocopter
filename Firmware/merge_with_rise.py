57cd37adefd1d9e5afd20dfe32020d7e570dc2be
Artem <62950291+vvoIfra@users.noreply.github.com>
2020-04-08 15:25:53 +0300

    Update merge_with_rise.py

--- Firmware/merge_with_rise.py
+++ Firmware/merge_with_rise.py
@@ -15,14 +15,10 @@ GREEN_COLOUR = (45, 135, 95)
 YELLOW_COLOUR = (215, 175, 40)
 RED_COLOUR = (255, 75, 75)
 WHITE_COLOUR = (255, 255, 255)
-DELTA = 75
+DELTA = 150
 
 rospy.init_node('simple_recognition')
 bridge = CvBridge()
-get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
-navigate = rospy.ServiceProxy('navigate', srv.Navigate)
-land = rospy.ServiceProxy('land', Trigger)
-
 # Раскоментить для публикации
 image_pub = rospy.Publisher('~debug', Image)
 
@@ -33,7 +29,7 @@ def image_callback(data):
     # Много штук для улучшения качества распознавания формы
     imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _, thrash = cv2.threshold(imgGrey, 240, 255, cv2.THRESH_BINARY)
-    _,contours, _ = cv2.findContours(
+    _, contours, _ = cv2.findContours(
         thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     for contour in contours:
@@ -49,16 +45,16 @@ def image_callback(data):
 
             x, y, w, h = cv2.boundingRect(contour)
             cropped_image = np.array(img[y:y + h, x:x + w])
-            main_colour = np.average(np.average(
+            main_color = np.average(np.average(
                 cropped_image, axis=0), axis=0)[::-1]
 
-            if abs(main_colour - GREEN_COLOUR).sum() < DELTA:
+            if abs(main_color - GREEN_COLOUR).sum() < DELTA:
                 print("Зелёный")
-            elif abs(main_colour - YELLOW_COLOUR).sum() < DELTA:
+            elif abs(main_color - YELLOW_COLOUR).sum() < DELTA:
                 print("Желтый")
-            elif abs(main_colour - RED_COLOUR).sum() < DELTA:
+            elif abs(main_color - RED_COLOUR).sum() < DELTA:
                 print("Красный")
-            elif abs(main_colour - WHITE_COLOUR).sum() < DELTA:
+            elif abs(main_color - WHITE_COLOUR).sum() < DELTA:
                 print("Белый")
     # Раскоментить для публикации
     image_pub.publish(bridge.cv2_to_imgmsg(img, 'bgr8'))
