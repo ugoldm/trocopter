f5493ebf70a9484bf10fedf64b7c2e2cf1f15c34
Artem <62950291+vvoIfra@users.noreply.github.com>
2020-04-08 15:24:18 +0300

    Update merge_with_rise.py

--- Firmware/merge_with_rise.py
+++ Firmware/merge_with_rise.py
@@ -28,13 +28,12 @@ image_pub = rospy.Publisher('~debug', Image)
 
 
 def image_callback(data):
-<<<<<<< Updated upstream
     img = bridge.imgmsg_to_cv2(data, 'bgr8')
 
     # Много штук для улучшения качества распознавания формы
     imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _, thrash = cv2.threshold(imgGrey, 240, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(
+    _,contours, _ = cv2.findContours(
         thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     for contour in contours:
@@ -64,59 +63,15 @@ def image_callback(data):
     # Раскоментить для публикации
     image_pub.publish(bridge.cv2_to_imgmsg(img, 'bgr8'))
 
-=======
-    cam = cv2.VideoCapture(0)
-    while True:
-        #img = bridge.imgmsg_to_cv2(data, 'bgr8')
-        ret_val,img = cam.read()
-        # Много штук для улучшения качества распознавания формы
-        imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        _, thrash = cv2.threshold(imgGrey, 240, 255, cv2.THRESH_BINARY)
-        contours, _ = cv2.findContours(
-            thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-        for contour in contours:
-            area = cv2.contourArea(contour)
-            if area > MIN_AREA and area < MAX_AREA:
-                approx = cv2.approxPolyDP(
-                    contour, 0.01 * cv2.arcLength(contour, True), True)
-                # Если нужна надпись
-                #x_title = approx.ravel()[0]
-                #y_title = approx.ravel()[1]
-
-                cv2.drawContours(img, [approx], 0, (0, 0, 0), 5)
-
-                x, y, w, h = cv2.boundingRect(contour)
-                cropped_image = np.array(img[y:y + h, x:x + w])
-                main_color = np.average(np.average(
-                    cropped_image, axis=0), axis=0)
-
-                if abs(main_color - GREEN_COLOUR).sum() < DELTA:
-                    print("Зелёный")
-                elif abs(main_color - YELLOW_COLOUR).sum() < DELTA:
-                    print("Желтый")
-                elif abs(main_color - RED_COLOUR).sum() < DELTA:
-                    print("Красный")
-                elif abs(main_color - WHITE_COLOUR).sum() < DELTA:
-                    print("Белый")
-        # Раскоментить для публикации
-        #image_pub.publish(bridge.cv2_to_imgmsg(img, 'bgr8'))
-        cv2.imshow("Result",img)
-        if cv2.waitKey(1) == 27: 
-            break  # esc to quit
-         cv2.destroyAllWindows()
->>>>>>> Stashed changes
 
 def main():
-    #image_sub = rospy.Subscriber(
-    #    'main_camera/image_raw', Image, image_callback)
-#
-    #navigate(x=0, y=0, z=1, speed=0.5, frame_id='body', auto_arm=True)
-    #rospy.sleep(10)
-    #land()
+    image_sub = rospy.Subscriber(
+        'main_camera/image_raw', Image, image_callback)
+    navigate(x=0, y=0, z=1, speed=0.5, frame_id='body', auto_arm=True)
+    rospy.sleep(10)
+    land()
 
-    #rospy.spin()
-    image_callback()
-
+    rospy.spin()
+    
 if __name__ == '__main__':
     main()
