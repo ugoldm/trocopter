5354cef09d74e82c71bed71e76279d437cff8efd
Artem <62950291+vvoIfra@users.noreply.github.com>
2020-04-09 11:36:46 +0300

    Update Barcode_with_rise.py

--- Firmware/Barcode_with_rise.py
+++ Firmware/Barcode_with_rise.py
@@ -7,12 +7,14 @@ from std_srvs.srv import Trigger
 
 bridge = CvBridge()
 rospy.init_node('barcode_test')
+allowable = ("COVID - 19","healthy","Non COVID - 19")
 
 #image_pub = rospy.Publisher('~debug', Image)
 get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
 navigate = rospy.ServiceProxy('navigate', srv.Navigate)
 land = rospy.ServiceProxy('land', Trigger)
 # Image subscriber callback function
+
 def image_callback_qr(data):
     cv_image = bridge.imgmsg_to_cv2(data, 'bgr8')  # OpenCV image
     barcodes = pyzbar.decode(cv_image)
@@ -22,15 +24,12 @@ def image_callback_qr(data):
         (x, y, w, h) = barcode.rect
         xc = x + w/2
         yc = y + h/2
-        print ("Found {} with data {} with center at x={}, y={}".format(b_type, b_data, xc, yc))
+        if b_data in allowable:
+	    cv2.putText(cv_image,b_data, (xc,yc))
+	    print(b_data)
+	image_pub.publish(bridge.cv2_to_imgmsg(cv_image, 'bgr8'))
 
-
-def main():
-	image_sub = rospy.Subscriber('main_camera/image_raw_throttled', Image, image_callback_qr, queue_size=1)
-	navigate(x=0, y=0, z=1, speed=0.3, frame_id='body', auto_arm=True)
-    rospy.sleep(10)
-    land()
-	rospy.spin()
-
-    if __name__ == '__main__':
-    main()
+image_sub = rospy.Subscriber('main_camera/image_raw_throttled', Image, image_callback_qr, queue_size=1)
+navigate(x=0, y=0, z=1, speed=0.3, frame_id='body', auto_arm=True)
+rospy.sleep(10)
+land()
