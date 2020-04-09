3f5d67762ece6070ba7c47acc03b84b5d44bdad1
ugoldm03 <ugoldm03@gmail.com>
2020-04-09 11:22:41 +0300

    add Barcode_with_rise

--- Firmware/Barcode_with_rise.py
+++ Firmware/Barcode_with_rise.py
@@ -2,11 +2,16 @@ import rospy
 from pyzbar import pyzbar
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from clever import srv
+from std_srvs.srv import Trigger
 
 bridge = CvBridge()
-
 rospy.init_node('barcode_test')
 
+#image_pub = rospy.Publisher('~debug', Image)
+get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
+navigate = rospy.ServiceProxy('navigate', srv.Navigate)
+land = rospy.ServiceProxy('land', Trigger)
 # Image subscriber callback function
 def image_callback_qr(data):
     cv_image = bridge.imgmsg_to_cv2(data, 'bgr8')  # OpenCV image
@@ -22,8 +27,10 @@ def image_callback_qr(data):
 
 def main():
 	image_sub = rospy.Subscriber('main_camera/image_raw_throttled', Image, image_callback_qr, queue_size=1)
-
+	navigate(x=0, y=0, z=1, speed=0.3, frame_id='body', auto_arm=True)
+    rospy.sleep(10)
+    land()
 	rospy.spin()
-	
-	if __name__ == '__main__':
+
+    if __name__ == '__main__':
     main()
