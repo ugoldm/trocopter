166816dec9d4651a059a80e191930dec425617a7
Artem <62950291+vvoIfra@users.noreply.github.com>
2020-04-09 13:37:19 +0300

    Update shape_detections.py

--- Firmware/shape_detections.py
+++ Firmware/shape_detections.py
@@ -1,4 +1,3 @@
-# coding: utf8
 import cv2
 import numpy as np
 import rospy
@@ -9,10 +8,14 @@ MIN_AREA = 400
 MAX_AREA = float("inf")
 
 GREEN_COLOUR = (65, 115, 85)
-YELLOW_COLOUR = (225, 195, 130)
+YELLOW_COLOUR = (175, 145, 58)
 RED_COLOUR = (165, 70, 75)
 WHITE_COLOUR = (255, 255, 255)
 DELTA = 43
+
+get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
+navigate = rospy.ServiceProxy('navigate', srv.Navigate)
+land = rospy.ServiceProxy('land', Trigger)
 
 rospy.init_node('detection')
 bridge = CvBridge()
@@ -39,22 +42,22 @@ def image_callback(data):
 
             cv2.drawContours(img, [approx], 0, (0, 0, 0), 5)
 
-            x, y, w, h=cv2.boundingRect(contour)
-            cropped_image=np.array(img[y:y + h, x:x + w])
-            main_color=np.average(np.average(
-                cropped_image, axis=0), axis = 0)[::-1]
+            min_delta = min(abs(main_color - GREEN_COLOUR).sum(),
+                            abs(main_color - YELLOW_COLOUR).sum(),
+                            abs(main_color - RED_COLOUR).sum())
 
-            if abs(main_color - GREEN_COLOUR).sum() < DELTA:
+            if abs(main_color - GREEN_COLOUR).sum() < DELTA and abs(main_color - GREEN_COLOUR).sum() == min_delta:
                 print("Зелёный")
                 cv2.putText(img, "Зелёный", (x + w, y + h))
 
-            elif abs(main_color - YELLOW_COLOUR).sum() < DELTA:
+            elif abs(main_color - YELLOW_COLOUR).sum() < DELTA and abs(main_color - YELLOW_COLOUR).sum() == min_delta:
                 print("Желтый")
                 cv2.putText(img, "Желтый", (x + w, y + h))
 
-            elif abs(main_color - RED_COLOUR).sum() < DELTA:
+            elif abs(main_color - RED_COLOUR).sum() < DELTA and abs(main_color - RED_COLOUR).sum() == min_delta:
                 print("Красный")
                 cv2.putText(img, "Красный", (x + w, y + h))
+
 
     # Раскоментить для публикации
     image_pub.publish(bridge.cv2_to_imgmsg(img, 'bgr8'))
@@ -62,7 +65,7 @@ def image_callback(data):
 
 def main():
     image_sub=rospy.Subscriber(
-        'main_camera/image_raw', Image, image_callback)
+        'main_camera/image_raw_throttled', Image, image_callback)
 
     rospy.spin()
 
